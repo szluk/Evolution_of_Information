777454d73b3f9ce86b66a87a2c355ff1688c748a
Szymon Łukaszyk <szymon@patent.pl>
2023-10-18 10:26:34 +0200

    Add files via upload

--- aufbau.m
+++ aufbau.m
@@ -11,30 +11,42 @@ clear all
 % History
 % v1: 16.10.2023 1st working version
 % v2: 17.10.2023 code simplification
-% v3: 18.10.2023 code simplification
+% v3: 18.10.2023 code simplification, looking for patterns...
 
-s2 = log2(2);
+s2 =   log2(2);
+p4 = 2*log2(2)                 -log2(3^(3/4));
+p5 =           -log2(2^(2/5))  -log2(3^(3/5))  +log2(5);
+d6 =   log2(2)                                 -log2(5^(5/6)) +log2(3);
+d7 =           -log2(2^(2/7))                  -log2(5^(5/7)) +log2(7);
+d8 = 3*log2(2)                 -log2(3^(3/8))  -log2(5^(5/8)); 
+d9 =           -log2(2^(8/9))                  -log2(5^(5/9)) +log2(9);
+f8 = 3*log2(2)                                                          -log2(7^(7/8));
+f9 =           -log2(2^(2/9))                                           -log2(7^(7/9))  +log2(9); 
+f10=   log2(2)                 -log2(3^(3/10))                          -log2(7^(7/10)) +log2(5);
+f11=           -log2(2^(8/11))                                          -log2(7^(7/11)) +log2(11);
+f12= 2*log2(2)                                 -log2(5^(5/12))          -log2(7^(7/12)) +log2(3);
+f13=           -log2(2^(6/13)) -log2(3^(6/13))                          -log2(7^(7/13)) +log2(13);
 
-p4 = 2*log2(2) - (3/4)*log2(3);
-p5 = log2(5) - (3/5)*log2(3) - 2*log2(2)/5;
+p4c = 2*log2(2) - (3/4)*log2(3);
+p5c =   log2(5) - (3/5)*log2(3) - 2*log2(2)/5;
 
-d6 = log2(6) - (5/6) *log2(5);
-d7 = log2(7) - (5/7) *log2(5) - 2*log2(2)/7;
-d8 = 3*log2(2) - (5/8) *log2(5) - 3*log2(3)/8;
-d9 = log2(9) - (5/9) *log2(5) - 4*log2(4)/9;
+d6c = log2(6) -(5/6) *log2(5);
+d7c = log2(7) -(5/7) *log2(5) - 2*log2(2)/7;
+d8c = 3*log2(2) -(5/8) *log2(5) - 3*log2(3)/8;
+d9c = log2(9) -(5/9) *log2(5) - 4*log2(4)/9;
 
-f8 = 3*log2(2) - (7/8) *log2(7);
-f9 = log2(9) - (7/9) *log2(7) - 2*log2(2)/9;
-f10= log2(2)+log2(5)- (7/10)*log2(7) - 3*log2(3)/10;
-f11= log2(11)- (7/11)*log2(7) - 4*log2(4)/11;
-f12= log2(12)- (7/12)*log2(7) - 5*log2(5)/12;
-f13= log2(13)- (7/13)*log2(7) - 6*log2(6)/13;
+f8c = 3*log2(2) -(7/8) *log2(7);
+f9c = log2(9) -(7/9) *log2(7) -2*log2(2)/9;
+f10c= log2(2) +log2(5)- (7/10)*log2(7) -3*log2(3)/10;
+f11c= log2(11) -(7/11)*log2(7) -4*log2(4)/11;
+f12c= log2(12) -(7/12)*log2(7) -5*log2(5)/12;
+f13c= log2(13) -(7/13)*log2(7) -6*log2(6)/13;
 
 EL={
 %Z,	'conf_act'		'conf_auf';
 1,	0,				0,	        'none';
-2,	s2,			    s2,	    '1st break';
-3,	s2,	  		    s2,	    'none';
+2,	s2,			    s2,	        '1st break';
+3,	s2,	  		    s2,	        'none';
 4,	2*s2,		    2*s2,	    'none';
 5,	2*s2,			2*s2,	    'none';
 6,	2*s2,			2*s2,	    'none';
@@ -55,12 +67,12 @@ EL={
 21,	6*s2,			6*s2,	    'none';
 22,	6*s2,			6*s2,	    'none';
 23,	6*s2,			6*s2,	    'none';
-24,	5*s2,			6*s2,	    'exc';
+24,	5*s2,			6*s2,	    'exc'; %-s2
 25,	6*s2,			6*s2,	    'none';
 26,	6*s2+d6,		6*s2+d6,	'none';
 27,	6*s2+d7,		6*s2+d7,	'none';
-28,	5*s2+d9,		6*s2+d8,	'exc disp';
-29,	6*s2,			6*s2+d9,	'exc';
+28,	5*s2+d9,		6*s2+d8,	'exc';%disp -d8+d9-s2
+29,	6*s2,			6*s2+d9,	'exc';%-d9
 30,	7*s2,			7*s2,	    'none';
 31,	7*s2,			7*s2,	    'none';
 32,	7*s2,			7*s2,	    'none';
@@ -72,8 +84,8 @@ EL={
 38,	9*s2,			9*s2,	    'none';
 39,	9*s2,			9*s2,	    'none';
 40,	9*s2,			9*s2,	    'none';
-41,	8*s2,			9*s2,	    'exc';
-42,	8*s2,			9*s2,	    'exc';
+41,	8*s2,			9*s2,	    'exc';%s2
+42,	8*s2,			9*s2,	    'exc';%s2
 43,	9*s2,			9*s2,	    'none';
 44,	8*s2+d7,		9*s2+d6,	'exc';
 45,	8*s2+d8,		9*s2+d7,	'exc';
@@ -149,7 +161,7 @@ EL={
 115,18*s2,		    18*s2,	    'predict';
 116,18*s2+p4,		18*s2+p4,	'predict';
 117,18*s2+p5,		18*s2+p5,	'predict';
-118,19*s2,		    19*s2,	    'predict'}
+118,19*s2,		    19*s2,	    'predict'};
 
 for k=1:size(EL, 1)
   ZZ(k) = EL{k,1};
@@ -165,10 +177,11 @@ plot(ZZ,HHH, 'r','LineWidth',linew)
 plot(ZZ,HH, 'g','LineWidth',linew)
 %plot(NiLineZ,NiLineen, 'g:','LineWidth',linew)
 set(gca,'FontName', 'Times New Roman')
-set(gca,'FontSize', 9)
+set(gca,'FontSize', 6)
 xlabel('Atomic number')
 ylabel('Shannon entropy (bits)')
-axis([0 118 0 19]) % bits
+%axis([0 118 0 19*log2(2)]) % bits
+axis([0 108 0 18*log2(2)]) % bits
 
 for k=1:size(EL, 1)
     if strcmp(EL{k,4}, 'exc')
@@ -178,6 +191,10 @@ for k=1:size(EL, 1)
           plot(ZZ(k), HH(k),'b+','LineWidth',linew)  
         end    
     end
+    if k == 46
+        plot(ZZ(k), HHH(k),'ro','LineWidth',linew)
+        plot(ZZ(k), HH(k), 'go','LineWidth',linew)        
+    end    
 end  
 
 line([2   2], [0 19], 'Color',[0 0 0], 'LineStyle', '-.');
@@ -187,13 +204,15 @@ line([36 36], [0 19], 'Color',[0 0 0], 'LineStyle', '-.');
 line([54 54], [0 19], 'Color',[0 0 0], 'LineStyle', '-.');
 line([86 86], [0 19], 'Color',[0 0 0], 'LineStyle', '-.');
 
+line([0 118], [9 9], 'Color',[0 0 0], 'LineStyle', '-.');
+
 % simplified formulas verification
 % Z S_act S_Auf H_act H_Auf
 TabEx= [24  6 4 5 *log2(2)                         6*log2(2);
         28  2 2  log2(2^(37/9) * 3^2 * 5^(-5/9))   log2(2^9 * 3^(-3/8) * 5^(-5/8) );
         29	1 1 6 *log2(2)                         log2(2^(46/9) * 3^2 * 5^(-5/9));
-        41  5 3 9 *log2(2)                         9*log2(2);
-        42  6 4 9 *log2(2)                         9*log2(2);
+        41  5 3 8 *log2(2)                         9*log2(2);
+        42  6 4 8 *log2(2)                         9*log2(2);
         44	4 4 log2(2^(54/7) * 5^(-5/7) * 7 )     log2(2^10 * 3 * 5^(-5/6));        
         45  3 3 log2(2^11 * 3^(-3/8) * 5^(-5/8))   log2(2^(61/7) * 5^(-5/7) * 7);        
         46  0 2 9 *log2(2)                         log2(2^12 * 3^(-3/8) * 5^(-5/8) );
